b39e44ae5e3c49b2b753f88a645193a05c069bd9
Moray Macdonald <moray.macdonald1@gmail.com>
2024-01-27 18:09:09 +0000

    Upgrade to astro-icons v1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 
 import sitemap from '@astrojs/sitemap';
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://cephid.world',
-	integrations: [sitemap()],
+	integrations: [sitemap(), icon()],
 });
