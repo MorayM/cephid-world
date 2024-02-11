fc99a6cd9bd096e4f8d338fd1fc9917f9499f720
Moray Macdonald <moray.macdonald1@gmail.com>
2024-02-11 13:17:13 +0000

    Spruce up content

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,6 @@
 
 export const SITE_TITLE = 'cephid.world';
 export const SITE_DESCRIPTION = 'Welcome to the world of Cephid';
+
+export const SPOTIFY_ALBUM_LINK = 'https://open.spotify.com/album/2EMZbtm2493l28hsNypQoF?si=y3vx1s9lRvWssXDe4AIzhg';
+export const BANDCAMP_ALBUM_LINK = 'https://cephid.bandcamp.com/album/sparks-in-the-darkness';
